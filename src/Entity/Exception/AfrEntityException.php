c7c7b06acc01054c500286ec9a29aea803c7e0f2
Nistor Alexandru Marius <djthorr@gmail.com>
2023-01-07 21:34:16 +0200

    test temp

---
+++ src/Entity/Exception/AfrEntityException.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+namespace Autoframe\Core\Entity\Exception;
+use Autoframe\Core\Exception\AutoframeException;
+
+class AfrEntityException  extends AutoframeException
+{
+
+}
--- src/FileSystem/Exception/AfrFileSystemException.php
+++ src/FileSystem/Exception/AfrFileSystemException.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 namespace Autoframe\Core\FileSystem\Exception;
 use Autoframe\Core\Exception\AutoframeException;
